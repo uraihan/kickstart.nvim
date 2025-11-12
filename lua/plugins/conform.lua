29cccd14f1dc643065e44917b60e535fba734d77
uraihan <umair.umer26@gmail.com>
2025-11-13 01:08:49 +0200

    disable treesitter auto-indenting (use guess-indent instead) and add djlint as html,css,jinja,etc. formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -32,6 +32,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        html = { 'djlint' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -294,6 +294,7 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'pylsp',
+        'djlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
