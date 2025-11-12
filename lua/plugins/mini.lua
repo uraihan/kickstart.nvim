207879c172685f88ac65102038a837fca94849a9
uraihan <umair.umer26@gmail.com>
2025-11-13 01:10:14 +0200

    added mini.move

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -19,6 +19,8 @@ return {
 
       require('mini.pairs').setup()
 
+      require('mini.move').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
