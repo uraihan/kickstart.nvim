788b43f715701f4769aa4165ea6dcbb5ae1d3153
uraihan <umair.umer26@gmail.com>
2025-11-27 01:59:44 +0200

    added keymap to open Oil from current window

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,6 +8,9 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   config = function()
     require('oil').setup()
+
+    --  Keymap to open Oil.nvim from current window
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
