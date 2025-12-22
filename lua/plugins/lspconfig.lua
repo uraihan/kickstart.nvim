2325e4e60c588c5ee204df9c7ec03566628f50a7
uraihan <umair.umer26@gmail.com>
2025-12-22 03:04:03 +0200

    fixed tailwindcss lsp not working on html or htmldjango files. NOTE: this only applies to flask-style project structure with static/ folder containing the css files

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -276,7 +276,22 @@ return {
           cssls = {},
 
           -- tailwind does not work for html or htmldjango files
-          tailwindcss = {},
+          tailwindcss = {
+            settings = {
+              tailwindCSS = {
+                experimental = {
+                  configFile = './static/daisy.css',
+                },
+              },
+            },
+            -- root_dir = function(bufnr, on_dir)
+            --   local root_files = { '.git' }
+            --   local fname = vim.api.nvim_buf_get_name(bufnr)
+            --   root_files = util.insert_package_json(root_files, 'tailwindcss', fname)
+            --   root_files = util.root_markers_with_field(root_files, { 'package.json', 'package-lock.json', 'mix.lock', 'Gemfile.lock' }, 'tailwind', fname)
+            --   on_dir(vim.fs.dirname(vim.fs.find(root_files, { path = fname, upward = true })[1]))
+            -- end,
+          },
           html = {
             filetypes = { 'html', 'htmldjango', 'templ' },
             settings = {},
